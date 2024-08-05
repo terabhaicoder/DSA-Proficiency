16f89a4feba1ebe8089465d1cc4bf6f6111b0577
Paarth Panthri <144156475+terabhaicoder@users.noreply.github.com>
2024-08-05 23:07:57 +0530

    Time: 3 ms (34.17%) | Memory: 8.1 MB (9.72%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int solve(int i,int j,vector<vector<int>>&dp){
+        if(i==0 && j==0) return dp[i][j] = 1;
+        if(i<0 || j<0) return 0;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up = solve(i-1,j,dp);
+        int left = solve(i,j-1,dp);
+
+        return dp[i][j] = up+left;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,dp);
+    }
+};
