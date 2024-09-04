e72dc9221e76409846c6596eeb3879d7017a7490
Paarth Panthri <144156475+terabhaicoder@users.noreply.github.com>
2024-09-04 11:30:59 +0530

    Time: 1790 ms (5.01%) | Memory: 178.7 MB (5.02%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.length();
+        int m = s2.length();
+        if(n>m) return false;
+        set<string>st;
+        for(int i=0;i<=m-n;i++){
+            string s = s2.substr(i,n);
+            sort(s.begin(),s.end());
+            st.insert(s);
+        }
+        sort(s1.begin(),s1.end());
+        if(st.find(s1)==st.end()) return false;
+        return true;
+    }
+};
