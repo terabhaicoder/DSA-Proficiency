5f0aebd268ed28cc2911290417bd963e47df93b4
Paarth Panthri <144156475+terabhaicoder@users.noreply.github.com>
2024-09-15 16:50:18 +0530

    Time: 21 ms (41.16%) | Memory: 29.9 MB (11.09%) - LeetSync

---
+++ 665-non-decreasing-array/non-decreasing-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        
+        int changed = false;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]<=nums[i+1]) continue;
+            if(changed) return false;
+
+            if(i==0 || nums[i+1]>=nums[i-1]){
+                nums[i] = nums[i+1];
+            }
+            else {
+                nums[i+1] = nums[i];
+            }
+            changed = true;
+
+        }
+        return true;
+    }
+};
