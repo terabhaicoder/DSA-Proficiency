fb97d5f6bd6527d63999ee7567b6486ee18aecc9
Paarth Panthri <144156475+terabhaicoder@users.noreply.github.com>
2024-09-28 12:54:19 +0530

    Time: 2122 ms (5.09%) | Memory: 7.4 MB (58.38%) - LeetSync

---
+++ 507-perfect-number/perfect-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum = 0;
+
+        for(int i=1;i<num-1;i++){
+            if(num%i==0) sum+=i;
+        }
+        if(sum==num) return true;
+        return false;
+    }
+};
